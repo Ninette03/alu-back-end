040841e962ce7651820c2ba7c63dc3927b653b9a
Ninette03 <n.agatesi@alustudent.com>
2024-06-28 12:09:21 +0200

    Updated file

--- api/0-gather_data_from_an_API.py
+++ api/0-gather_data_from_an_API.py
@@ -1,21 +1,25 @@
 #!/usr/bin/python3
 import requests
+import json
 
 USER_ID = 1
-USER_URL = f"https://jsonplaceholder.typicode.com/users/${USER_ID}"
-TODO_URL = f"https://jsonplaceholder.typicode.com/todos"
+USER_URL = f'https://jsonplaceholder.typicode.com/users/${USER_ID}'
+TODO_URL = f'https://jsonplaceholder.typicode.com/todos'
 
 employee = requests.get(USER_URL).json()
 todos = requests.get(TODO_URL).json()
 
 todo_counter = 0
 completed_counter = 0
+completed_tasks =[]
 for todo in todos:
-    if todo['userId'] == 1:
+    if todo['userId'] == USER_ID:
         todo_counter += 1
         if todo['completed'] is True:
             completed_counter += 1
+            completed_tasks.append(todo['title'])
 
-print(employee['name'])
-print(todo_counter)
-print(completed_counter)
+employee_name = employee['name']
+print(f"Employee {employee_name} is done with tasks({completed_counter}/{todo_counter})")
+for title in completed_tasks:
+    print(title)
